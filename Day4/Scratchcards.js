0f166fc7b17844e4f4e69a61d4df7bf84aab13c8
anh bien <anhnbien@gmail.com>
2023-12-04 13:09:23 -0600

    Updated day 4

--- Day4/Scratchcards.js
+++ Day4/Scratchcards.js
@@ -1,88 +1,97 @@
-// // Requiring the fs module
-// const fs = require("fs")
-
-// // Creating a function which takes a file as input
-// const readFileLines = filename =>
-//     fs
-//         .readFileSync(filename)
-//         .toString('UTF8')
-//         .split('\n');
-
-// // Driver code
-// let data = readFileLines('value.txt');
-
-// const scratchCards = () => {
-//     let total = 0
-//     let winingInstances = new Array(data.length).fill(1)
-//     data.forEach((row, index) => {
-//         const line = row.slice(row.indexOf(': ') + 2).split('|')
-//         const winningNumbers = line[0].trim().split(' ').filter(val => !isNaN(val) || val !== '')
-//         const lotteryNumbers = line[1].trim().split(' ').filter(val => !isNaN(val) || val !== '')
-//         const winning = lotteryNumbers.filter(num => winningNumbers.includes(num)).filter(val => val !== '')
-//         if (winning.length > 0) {
-//             total += Math.pow(2, winning.length - 1)
-//         }
-//         winning.forEach((_, i) => {
-//             winingInstances[index + i + 1] += winingInstances[index]
-//         })
-//     })
-//     // console.log(winingInstances)
-//     console.log('P1: ' + total)
-//     console.log('P2: ' + winingInstances.reduce((a, b) => a + b))
-// }
-
-// scratchCards()
-
+// Requiring the fs module
 const fs = require("fs")
 
-// Function to read lines from a file
-function readLinesFromFile(filename) {
-  try {
-    const fileContent = fs.readFileSync(filename, "utf8");
-    return fileContent.split("\n");
-  } catch (error) {
-    console.error("Error reading file:", error.message);
-    return [];
-  }
+// Creating a function which takes a file as input
+const readFileLines = filename =>
+    fs
+        .readFileSync(filename)
+        .toString('UTF8')
+        .split('\n');
+
+// Driver code
+let data = readFileLines('value.txt');
+
+const scratchCards = () => {
+    let total = 0
+    let winingInstances = new Array(data.length).fill(1)
+    data.forEach((row, index) => {
+        // Process input data
+        const line = row.slice(row.indexOf(': ') + 2).split('|')
+        const winningNumbers = line[0].trim().split(' ').filter(val => !isNaN(val) || val !== '')
+        const lotteryNumbers = line[1].trim().split(' ').filter(val => !isNaN(val) || val !== '')
+
+        // Find winning numbers
+        const winning = lotteryNumbers.filter(num => winningNumbers.includes(num)).filter(val => val !== '')
+
+        if (winning.length > 0) {
+            // Part 1
+            total += Math.pow(2, winning.length - 1)
+
+            // Part 2
+            winning.forEach((_, i) => {
+                winingInstances[index + i + 1] += winingInstances[index]
+            })
+        }
+    })
+    // console.log(winingInstances)
+    console.log('P1: ' + total)
+    console.log('P2: ' + winingInstances.reduce((a, b) => a + b))
 }
 
-// Function to calculate the total prize and winning instances
-function calculatePrizes(lines) {
-  let totalPrize = 0;
-  const winningInstances = new Array(lines.length).fill(1);
+scratchCards()
 
-  lines.forEach((line, index) => {
-    const [winningNumbersStr, lotteryNumbersStr] = line.split("|").map((str) => str.trim());
-    const winningNumbers = winningNumbersStr.split(" ").filter((val) => !isNaN(val) || val !== "");
-    const lotteryNumbers = lotteryNumbersStr.split(" ").filter((val) => !isNaN(val) || val !== "");
 
-    const matchingNumbers = lotteryNumbers.filter((num) => winningNumbers.includes(num)).filter((val) => val !== "");
+// Better?
+// const fs = require("fs")
 
-    if (matchingNumbers.length > 0) {
-      totalPrize += 2 ** (matchingNumbers.length - 1);
-    }
+// // Function to read lines from a file
+// function readLinesFromFile(filename) {
+//   try {
+//     const fileContent = fs.readFileSync(filename, "utf8");
+//     return fileContent.split("\n");
+//   } catch (error) {
+//     console.error("Error reading file:", error.message);
+//     return [];
+//   }
+// }
 
-    matchingNumbers.forEach((_, i) => {
-      winningInstances[index + i + 1] += winningInstances[index];
-    });
-  });
+// // Function to calculate the total prize and winning instances
+// function calculatePrizes(lines) {
+//   let totalPrize = 0;
+//   const winningInstances = new Array(lines.length).fill(1);
 
-  return { totalPrize, winningInstances };
-}
+//   lines.forEach((line, index) => {
+//     const [winningNumbersStr, lotteryNumbersStr] = line.split("|").map((str) => str.trim());
+//     const winningNumbers = winningNumbersStr.split(" ").filter((val) => !isNaN(val) || val !== "");
+//     const lotteryNumbers = lotteryNumbersStr.split(" ").filter((val) => !isNaN(val) || val !== "");
 
-function main() {
-  const filename = "value.txt";
-  const lines = readLinesFromFile(filename);
+//     const matchingNumbers = lotteryNumbers.filter((num) => winningNumbers.includes(num)).filter((val) => val !== "");
 
-  if (lines.length === 0) {
-    console.log("No data to process.");
-    return;
-  }
+//     if (matchingNumbers.length > 0) {
+//       totalPrize += 2 ** (matchingNumbers.length - 1);
+//     }
 
-  const { totalPrize, winningInstances } = calculatePrizes(lines);
+//     matchingNumbers.forEach((_, i) => {
+//       winningInstances[index + i + 1] += winningInstances[index];
+//     });
+//   });
 
-  console.log("P1:", totalPrize);
-  console.log("P2:", winningInstances.reduce((a, b) => a + b));
-}
+//   return { totalPrize, winningInstances };
+// }
 
-main();
+// function main() {
+//   const filename = "value.txt";
+//   const lines = readLinesFromFile(filename);
+
+//   if (lines.length === 0) {
+//     console.log("No data to process.");
+//     return;
+//   }
+
+//   const { totalPrize, winningInstances } = calculatePrizes(lines);
+
+//   console.log("P1:", totalPrize);
+//   console.log("P2:", winningInstances.reduce((a, b) => a + b));
+// }
+
+// main();
